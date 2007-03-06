e7dcbf49f956f55ac3770160e1be14265197f479
no_author <no_author@no_author>
2007-03-06 18:15:30 +0000

    cleaned up more

--- plumeControl.h
+++ plumeControl.h
@@ -4,6 +4,8 @@
 #include "particleControl.h"
 #include "displayControl.h"
 #include "particleEmitter.h"
+#include "framebufferObject.h"
+#include "renderbuffer.h"
 #include "GLSL.h"
 #include <list>
 
@@ -15,9 +17,11 @@ class PlumeControl{
   PlumeControl(int, int, int);
 
   void init();
-  void injectParticles(FramebufferObject*, bool);
-  void advectParticles(FramebufferObject*, bool);
-  void displayVisual(GLuint);
+ 
+  void display();
+  //void injectParticles(FramebufferObject*, bool);
+  //void advectParticles(FramebufferObject*, bool);
+  //void displayVisual(GLuint);
   
  
   float time_step; //time step used for the movement of particles
@@ -32,15 +36,33 @@ class PlumeControl{
   GLuint texid[8]; 
   GLenum texType;
   
+  FramebufferObject* fbo;
+  Renderbuffer* rb;
+  GLuint vertex_buffer;
+
+  bool dump_contents;
+  bool emit;
+  bool show_particle_visuals;
+
+ 
+ 
 
  private:
 
   void setupTextures();
+  void initFBO();
       
+  Timer* display_clock;
+  Timer_t display_time[2];
+
+  bool odd;
+
   int testcase;
   GLSLObject emit_shader;
   GLenum int_format; 
   GLenum int_format_init;
+
+  GLint draw_buffer;
   
 
 };
