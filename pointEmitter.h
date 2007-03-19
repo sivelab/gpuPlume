d7fe0e159d3fa6499c83e818ec8737374df05435
no_author <no_author@no_author>
2007-03-19 18:47:22 +0000

    new class

---
+++ pointEmitter.h
@@ -0,0 +1,19 @@
+#ifndef __POINT_EMITTER_H__
+#define __POINT_EMITTER_H__
+
+#include "particleEmitter.h"
+
+class PointEmitter : public ParticleEmitter{
+
+ public:
+
+  PointEmitter(float,float,float,float,int*,int*,std::list<int>*, GLSLObject*);
+  virtual void EmitParticle(FramebufferObject*, bool);
+  //virtual void Draw();
+
+ protected:
+  virtual ~PointEmitter();
+
+};
+
+#endif //__POINT_EMITTER_H__
