68a08158453bce221ae547bc4212453afa2ecba1
no_author <no_author@no_author>
2007-03-23 20:30:08 +0000

    fixed error

--- collectionBox.cpp
+++ collectionBox.cpp
@@ -6,10 +6,10 @@ CollectionBox::CollectionBox(int x,int y,int z,float* bounds){
   numBox_z = z;
   
   lx = bounds[0];
-  ux = bounds[1];
-  ly = bounds[2];
-  uy = bounds[3];
-  lz = bounds[4];
+  ly = bounds[1];
+  lz = bounds[2];
+  ux = bounds[3];
+  uy = bounds[4];
   uz = bounds[5];
   
   cBox = new cell[x*y*z];
@@ -18,11 +18,13 @@ CollectionBox::CollectionBox(int x,int y,int z,float* bounds){
     cBox[i].count = 0;
   }
   n = 0;
+
   
 }
 void CollectionBox::seeIfInBox(float x, float y, float z){
 
   if( (lx <= x)&&(x <= ux)&&(ly <= y)&&(y <= uy)&&(lz <= z)&&(z <= uz) ){
+   
     int xBox = (int)floor((x-lx)/((ux-lx)/numBox_x));
     int yBox = (int)floor((y-ly)/((uy-ly)/numBox_y));
     int zBox = (int)floor((z-lz)/((uz-lz)/numBox_z));
@@ -32,14 +34,22 @@ void CollectionBox::seeIfInBox(float x, float y, float z){
 
 }
 void CollectionBox::calculateAvg(){
-  alpha = n*(n/1.0);
+  alpha = n/(n+1.0);
+  //std::cout << alpha << std::endl;
 
   for(int i = 0; i < numBox_x*numBox_y*numBox_z; i++){
-    cBox[i].moving_avg = alpha*cBox[i].moving_avg + (1-alpha)*cBox[i].count;
+    cBox[i].moving_avg = alpha*cBox[i].moving_avg + (1.0-alpha)*cBox[i].count;
     
     cBox[i].count = 0;
 
   }
   n++;
+
+}
+void CollectionBox::outputAvg(){
+   for(int i = 0; i < numBox_x*numBox_y*numBox_z; i++){
+     std::cout << cBox[i].moving_avg << std::endl;
+
+  }
 
 }
