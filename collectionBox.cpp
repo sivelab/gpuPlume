55bce0762b0a7903441a94a9291813cafd96a265
no_author <no_author@no_author>
2007-03-23 19:50:23 +0000

    collectionBox avg

--- collectionBox.cpp
+++ collectionBox.cpp
@@ -1,5 +1,4 @@
 #include "collectionBox.h"
-#include <math.h>
 
 CollectionBox::CollectionBox(int x,int y,int z,float* bounds){
   numBox_x = x;
@@ -13,16 +12,34 @@ CollectionBox::CollectionBox(int x,int y,int z,float* bounds){
   lz = bounds[4];
   uz = bounds[5];
   
-  cBox = new int[x*y*z];
+  cBox = new cell[x*y*z];
+  for(int i = 0; i < numBox_x*numBox_y*numBox_z; i++){
+    cBox[i].moving_avg = 0;    
+    cBox[i].count = 0;
+  }
+  n = 0;
   
 }
 void CollectionBox::seeIfInBox(float x, float y, float z){
+
   if( (lx <= x)&&(x <= ux)&&(ly <= y)&&(y <= uy)&&(lz <= z)&&(z <= uz) ){
     int xBox = (int)floor((x-lx)/((ux-lx)/numBox_x));
     int yBox = (int)floor((y-ly)/((uy-ly)/numBox_y));
     int zBox = (int)floor((z-lz)/((uz-lz)/numBox_z));
     int idx = yBox*numBox_x*numBox_z + xBox*numBox_z + zBox;
-    cBox[idx]++;
+    cBox[idx].count++;     
   }
-  
+
+}
+void CollectionBox::calculateAvg(){
+  alpha = n*(n/1.0);
+
+  for(int i = 0; i < numBox_x*numBox_y*numBox_z; i++){
+    cBox[i].moving_avg = alpha*cBox[i].moving_avg + (1-alpha)*cBox[i].count;
+    
+    cBox[i].count = 0;
+
+  }
+  n++;
+
 }
