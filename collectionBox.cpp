9f912698222b6f57cb7ee38cee652dfa2898d349
no_author <no_author@no_author>
2007-03-23 19:10:30 +0000

    collectionBox beginning

---
+++ collectionBox.cpp
@@ -0,0 +1,28 @@
+#include "collectionBox.h"
+#include <math.h>
+
+CollectionBox::CollectionBox(int x,int y,int z,float* bounds){
+  numBox_x = x;
+  numBox_y = y;
+  numBox_z = z;
+  
+  lx = bounds[0];
+  ux = bounds[1];
+  ly = bounds[2];
+  uy = bounds[3];
+  lz = bounds[4];
+  uz = bounds[5];
+  
+  cBox = new int[x*y*z];
+  
+}
+void CollectionBox::seeIfInBox(float x, float y, float z){
+  if( (lx <= x)&&(x <= ux)&&(ly <= y)&&(y <= uy)&&(lz <= z)&&(z <= uz) ){
+    int xBox = (int)floor((x-lx)/((ux-lx)/numBox_x));
+    int yBox = (int)floor((y-ly)/((uy-ly)/numBox_y));
+    int zBox = (int)floor((z-lz)/((uz-lz)/numBox_z));
+    int idx = yBox*numBox_x*numBox_z + xBox*numBox_z + zBox;
+    cBox[idx]++;
+  }
+  
+}
