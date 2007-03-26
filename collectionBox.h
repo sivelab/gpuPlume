7b90ba58a0c8b05f20359612b4ac4aa7da20955f
no_author <no_author@no_author>
2007-03-26 18:27:03 +0000

    commented

--- collectionBox.h
+++ collectionBox.h
@@ -12,20 +12,35 @@ typedef struct{
 class CollectionBox{
  public:
 
+  
+  //The first three int variables are the number of boxes in
+  //the x,y, and z directions respectively.
+  //The array of floats is the two coordinate bounds of the cuboid.
+  //The first three values is the lower x,y,z coordinate
+  //and the last three is the upper x,y,z coordinate
   CollectionBox(int,int,int,float*);
   
+  //Checks the location of the particles each pass.
+  //If a particle is inside the collection box, it will add
+  //one to the particle count for the indexed position.  
   void seeIfInBox(float,float,float);
+
+  //Calculates the moving Average for each position in the 
+  //collection box
   void calculateAvg();
+
+  //Writes the moving average values to standard output.
   void outputAvg();
 
   cell* cBox;
 
  private:
 
-  int numBox_x;
-  int numBox_y;
-  int numBox_z;
+  int numBox_x; //number of boxes in the x direction
+  int numBox_y; //number of boxes in the y direction
+  int numBox_z; //number of boxes in the z direction
 
+  //l - lower; u - upper
   float lx;
   float ux;
   float ly;
@@ -33,7 +48,9 @@ class CollectionBox{
   float lz;
   float uz;
 
+  //used to keep track of the number of samples taken
   double n;
+  //alpha value used in calculating moving average
   double alpha;
 
 };
