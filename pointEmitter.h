79cf0d205cf907fa37a26b15dae573ce27a28c11
no_author <no_author@no_author>
2007-03-29 18:47:48 +0000

    fixed concentration calc

--- pointEmitter.h
+++ pointEmitter.h
@@ -8,7 +8,7 @@ class PointEmitter : public ParticleEmitter{
  public:
 
   PointEmitter(float,float,float,float,int*,int*,std::list<int>*, GLSLObject*);
-  virtual void EmitParticle(FramebufferObject*, bool);
+  virtual int EmitParticle(FramebufferObject*, bool);
   //virtual void Draw();
 
  protected:
