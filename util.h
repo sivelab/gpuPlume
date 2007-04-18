7b7240d55e8f53220bff91ca826d080da804b3bc
no_author <no_author@no_author>
2007-04-18 16:43:16 +0000

    values added to input file

--- util.h
+++ util.h
@@ -19,6 +19,7 @@ class Util{
   void parseLine(char*);
   bool read1Float(char*,std::string,float*);
   bool read6Float(char*,std::string,float*);
+  bool read4Float(char*,std::string,float*);
   bool readComment(const char*);
   bool read1String(const char*,char*,std::string*);
   
