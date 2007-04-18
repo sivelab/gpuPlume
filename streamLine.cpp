f2f1d33c5876d24f0183a57cf6b9ef9b169c264a
no_author <no_author@no_author>
2007-04-18 19:52:08 +0000

    made it better

--- streamLine.cpp
+++ streamLine.cpp
@@ -1,4 +1,5 @@
 #include "streamLine.h"
+#include <math.h>
 
 StreamLine::StreamLine(int w, int h,int x,int y,int z){
   twidth = w;
@@ -33,6 +34,7 @@ void StreamLine::addNewStream(ParticleEmitter* pe){
    streamNum++;
    
    startStream = true;
+   update = true;
 }
 
 void StreamLine::updateStreamPos(){
@@ -43,19 +45,24 @@ void StreamLine::updateStreamPos(){
    indexIter = indexList.begin();
    while(indexIter != indexList.end()){
      sIndex = *indexIter;
+     if(!sIndex.done){
 
-     int idx = sIndex.t*twidth*4 + sIndex.s*4;
-     pos.x = pos_buffer[idx];
-     pos.y = pos_buffer[idx+1];
-     pos.z = pos_buffer[idx+2];
+       streamIndex &sIdx = *indexIter;
+
+       int idx = sIndex.t*twidth*4 + sIndex.s*4;
+       pos.x = pos_buffer[idx];
+       pos.y = pos_buffer[idx+1];
+       pos.z = pos_buffer[idx+2];
        
-     partPos prev = streamList[sIndex.i].back();
-     //if((prev.x != pos.x) || (prev.y != pos.y) || (prev.z != pos.z))
-     if((pos.x >= nx) || (pos.y >= ny) || (pos.z >= nz))
-       sIndex.done = true;
-     else
-       streamList[sIndex.i].push_back(pos);
+       partPos prev = streamList[sIndex.i].back();
+       //if((prev.x != pos.x) || (prev.y != pos.y) || (prev.z != pos.z))
+       if((pos.x >= nx) || (pos.y >= ny) || (pos.z >= nz)){
+	 sIdx.done = true;
+       }
+       else
+	 streamList[sIndex.i].push_back(pos);
 
+     }
      indexIter++;
    }
      
@@ -69,15 +76,21 @@ void StreamLine::draw(){
 
   //This is just so I don't get a warning after compiling
   pos.x = pos.y = pos.z = 0.0;
+  float colorx,colory,colorz;
 
   for(int i =0;i < (int)streamList.size(); i++){
     for(int j = 0; j < (int)streamList[i].size(); j++){
       prevPos = pos;
       pos = streamList[i][j];
 
+      //colorx = 1/fabs(prevPos.x - pos.x);
+      //colory = 1/fabs(prevPos.y-pos.y);
+      //colorz = 1/fabs(prevPos.z-pos.z);
+
       if(!first){
 	glBegin(GL_LINES);
 	glColor3f(1.0,1.0,0.0);
+	//glColor3f(colorx,colory,colorz);
 	glVertex3f(prevPos.x,prevPos.y,prevPos.z);
 	glVertex3f(pos.x,pos.y,pos.z);
 	glEnd();    
