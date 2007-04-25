bebe0c2c9dbed024f9d0d3b7ab1b7e5bee644bd7
no_author <no_author@no_author>
2007-04-25 21:14:40 +0000

    changed

--- simulation.h
+++ simulation.h
@@ -11,6 +11,7 @@ class Simulation{
   void init();
   void setStartTime();
   bool update(float*);
+  float randVal();
 
   //holds value for total elapsed time in running collection boxes
   double totalTime;
