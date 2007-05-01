922d549cf569aa8ca9e0e8cab0bb63ffd17dd765
no_author <no_author@no_author>
2007-05-01 19:58:06 +0000

    Added print out for mean, std, and var for random variables.

--- plumeControl.cpp
+++ plumeControl.cpp
@@ -575,6 +575,7 @@ void PlumeControl::setupTextures()
 	CheckErrorsGL("\tcreated texid[1], the position texture (double buffer)...");
 
 
+	std::vector<float> random_values;
 	//These two textures are to store the prime values(previous and updated values)
 	//We will need to initialize some data into prime0
 	for (int j=0; j<theight; j++)
@@ -586,6 +587,11 @@ void PlumeControl::setupTextures()
 	      data[idx+1] = Random::normal();
 	      data[idx+2] = Random::normal();
 	      data[idx+3] = 0.0;
+
+	      // record the random values to compute a mean, std, and var
+	      random_values.push_back( data[idx] );
+	      random_values.push_back( data[idx + 1] );
+	      random_values.push_back( data[idx + 2] );
 
 	      // normalize
 	      float mag = sqrt(data[idx]*data[idx] + data[idx+1]*data[idx+1] 
@@ -601,6 +607,26 @@ void PlumeControl::setupTextures()
 	      //data[idx+2] = sigU*(data[idx+2]/mag);
 	    }
 
+	// Sum random values to determine if they have mean of zero and variance of 1
+	double mean = 0.0;
+	// std::cout << "randvalsprime = [" << std::endl;
+	for (unsigned int i=0; i<random_values.size(); i++)
+	  {
+	    // std::cout << random_values[i] << std::endl;
+	    mean += random_values[i];
+	  }
+	// std::cout << "];" << std::endl;
+	mean = mean / (double)random_values.size();
+
+	double stddev = 0.0, variance = 0.0, tmp_sum = 0.0;
+	for (unsigned int i=0; i<random_values.size(); i++)
+	  {
+	    tmp_sum += ((random_values[i] - mean) * (random_values[i] - mean));
+	  }
+	variance = tmp_sum / (double)random_values.size();
+	stddev = sqrt( variance );
+	std::cout << "Prime Textures Random Values: Mean = " << mean << ", Standard Deviation = " << stddev << ", Variance = " << variance << std::endl;
+
 	pc->createTexture(prime0, int_format, twidth, theight, data);
 	CheckErrorsGL("\tcreated texid[5], the initial prime value texture...");
 
@@ -609,6 +635,7 @@ void PlumeControl::setupTextures()
 	//
 	// create random texture for use with particle simulation and turbulence
 	//
+	random_values.clear();
 	for (int j=0; j<theight; j++)
 	  for (int i=0; i<twidth; i++)
 	    {
@@ -623,7 +650,31 @@ void PlumeControl::setupTextures()
 		data[idx+1] = Random::normal();
 		data[idx+2] = Random::normal();
 		data[idx+3] = 0.0;
+		
+		random_values.push_back( data[idx] );
+		random_values.push_back( data[idx + 1] );
+		random_values.push_back( data[idx + 2] );
 	    }
+
+	// Sum random values to determine if they have mean of zero and variance of 1
+	mean = 0.0;
+	for (unsigned int i=0; i<random_values.size(); i++)
+	  {
+	    mean += random_values[i];
+	  }
+	mean = mean / (double)random_values.size();
+
+	stddev = 0.0;
+	variance = 0.0; 
+	tmp_sum = 0.0;
+	for (unsigned int i=0; i<random_values.size(); i++)
+	  {
+	    tmp_sum += ((random_values[i] - mean) * (random_values[i] - mean));
+	  }
+	variance = tmp_sum / (double)random_values.size();
+	stddev = sqrt( variance );
+	std::cout << "texid[4] Random Values: Mean = " << mean << ", Standard Deviation = " << stddev << ", Variance = " << variance << std::endl;
+
 	pc->createTexture(texid[4], int_format, twidth, theight, data);
 	CheckErrorsGL("\tcreated texid[4], the random number texture...");
 
