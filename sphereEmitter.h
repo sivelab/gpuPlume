ef3e128773d5631a6840d78b3d9375480db64bab
no_author <no_author@no_author>
2007-05-09 16:57:26 +0000

    simplified particle emitter

--- sphereEmitter.h
+++ sphereEmitter.h
@@ -3,15 +3,15 @@
 
 #include "particleEmitter.h"
 #include <GL/glut.h>
-#include "Random.h"
 
 class SphereEmitter : public ParticleEmitter{
 
  public:
 
-  SphereEmitter(float,float,float,float,float,int*,int*,std::list<int>*, GLSLObject*);
+  SphereEmitter(float,float,float,float,float,int,int,std::list<int>*,GLSLObject*);
 
-  virtual int EmitParticle(FramebufferObject*, bool);
+  //virtual int EmitParticle(FramebufferObject*, bool);
+  virtual void setVertices();
   virtual void Draw();
   //virtual void getPosition(float*,float*,float*,float*);
   virtual void getReleasedPosition(float*,float*,float*);
