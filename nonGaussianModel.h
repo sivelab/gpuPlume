e05b4ccf18243dbf030b84cb9d74d0dd3e1eb765
no_author <no_author@no_author>
2007-07-23 20:45:16 +0000

    new class structure for dispersion models

---
+++ nonGaussianModel.h
@@ -0,0 +1,24 @@
+#ifndef __nonGaussianModel_H__
+#define __nonGaussianModel_H__
+
+#include "plumeControl.h"
+
+class NonGaussianModel : public PlumeControl{
+
+ public:
+  
+  NonGaussianModel(Util*);
+  virtual void init(bool);
+  virtual int display();
+  virtual void setupTextures();
+  virtual void setupEmitters();
+  
+ protected:
+
+  virtual ~NonGaussianModel();
+
+
+
+};
+
+#endif //__nonGaussianModel_H__
