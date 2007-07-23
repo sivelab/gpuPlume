9287c0b75990a4f8a8fd1d030a1e9e996d3a9bad
no_author <no_author@no_author>
2007-07-23 20:44:59 +0000

    new class structure for dispersion models

---
+++ GaussianModel.h
@@ -0,0 +1,24 @@
+#ifndef __GaussianModel_H__
+#define __GaussianModel_H__
+
+#include "plumeControl.h"
+
+class GaussianModel : public PlumeControl{
+
+ public:
+  
+  GaussianModel(Util*);
+  virtual void init(bool);
+  virtual int display();
+  virtual void setupTextures();
+  virtual void setupEmitters();
+  
+ protected:
+
+  virtual ~GaussianModel();
+
+
+
+};
+
+#endif //__GaussianModel_H__
