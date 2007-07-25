393dd779f44a29e6fd84f81a9aecbe218c6c5378
no_author <no_author@no_author>
2007-07-25 15:52:41 +0000

    changed organization

--- pointEmitter.cpp
+++ pointEmitter.cpp
@@ -38,3 +38,115 @@ void PointEmitter::setVertices(){
     
   }
 }
+int PointEmitter::EmitParticle(bool odd,GLuint pos0, GLuint pos1,
+			       float time_step){
+  int p_index;
+
+  switch(releaseType){
+  case perSecond:
+    if(!timeToEmit(time_step))
+	numToEmit = 0;
+    break;
+  case onePerKeyPress:
+    setNumToEmit(1);
+    emit = false;
+    break;
+  case instantaneous:
+    setNumToEmit(twidth*theight);
+    emit = false;
+    break;
+  default:
+    //This will release per time step with numToEmit already defined!!!
+    break;
+  }
+
+  if(!indices->empty()){
+    //THIS Method *seems* to work now!
+    //Punch Hole method. Need to set drawbuffer and activate shader.
+    if(Punch_Hole){
+
+      if(odd)
+	glDrawBuffer(GL_COLOR_ATTACHMENT0_EXT);
+      else 
+	glDrawBuffer(GL_COLOR_ATTACHMENT1_EXT);
+
+      glMatrixMode(GL_PROJECTION);
+      glLoadIdentity();
+      gluOrtho2D(0, twidth, 0, theight);
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();     
+
+    }
+    	
+    //Do this for each particle that is being emitted.
+    for(int i = 0; i < numToEmit; i++){
+      if(!indices->empty()){
+	
+	//First get available index
+	p_index = indices->back();
+	indices->pop_back();
+	if(reuse){
+	  pIndex newIndex;
+	  newIndex.id = p_index;
+	  newIndex.time = 0;
+	  indicesInUse->push_back(newIndex);	   
+	}	 
+
+	//Determine the coordinates into the position texture
+	s = (p_index%twidth);
+	t = (p_index/twidth);	  
+
+	if(Punch_Hole){
+	  glPointSize(1.0);
+
+	  shader->activate();
+
+	  glViewport(s,t,1,1);
+	  //std::cout << "particle num= " << p_index << "  s = " << s << "  t = " << t << std::endl;
+	  glBegin(GL_POINTS);
+	  {
+	    glColor4f(xpos, ypos, zpos, lifeTime);
+	    glVertex2f(0.5, 0.5);
+	    //glVertex2f(s,t);
+	  }
+	  glEnd();
+	  shader->deactivate();
+
+	}
+	else{
+	  // Second mechanism to release particles.  Uses a texture
+	  // copy which may likely be more expensive even though we're
+	  // doing 1x1 pixels (but many times).  This operation
+	  // appears to work consistently.
+	  GLfloat value[4];
+	  value[0] = xpos;
+	  value[1] = ypos;
+	  value[2] = zpos;
+	  value[3] = lifeTime;
+
+	  // write there via a glTexSubImage2D
+	  if(odd)
+	    // will read from this texture next
+	    glBindTexture(GL_TEXTURE_RECTANGLE_ARB, pos0);
+	  else 
+	    // will read from this texture next
+	    glBindTexture(GL_TEXTURE_RECTANGLE_ARB, pos1);
+
+	  glTexSubImage2D(GL_TEXTURE_RECTANGLE_ARB, 0, s, t, 1, 1, GL_RGBA, GL_FLOAT, value);
+	}
+  
+      }
+    }
+
+    if(Punch_Hole){
+      glMatrixMode(GL_PROJECTION);
+      glLoadIdentity();
+      gluOrtho2D(-1, 1, -1, 1);
+      glMatrixMode(GL_MODELVIEW);
+      glLoadIdentity();
+    }    
+
+  }
+  return numToEmit;
+   
+}
