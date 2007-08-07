343ccf39f5873d3363af581e89e186110553ce58
no_author <no_author@no_author>
2007-08-07 20:16:08 +0000

    new shader to calculate mean prime

---
+++ Shaders/meanVel_fp.glsl
@@ -0,0 +1,28 @@
+uniform samplerRect prevMean;
+uniform samplerRect currVel;
+uniform samplerRect position;
+
+uniform int nx;
+uniform int ny;
+uniform int nz;
+
+void main(void)
+{
+   vec2 texCoord = gl_TexCoord[0].xy;
+   vec3 velocity = vec3(textureRect(currVel, texCoord));
+   vec4 mean = vec4(textureRect(prevMean, texCoord));
+   vec3 pos = vec3(textureRect(position, texCoord));
+
+
+
+   int i = floor(pos.y); 
+   int j = floor(pos.x);
+   int k = floor(pos.z);
+
+   if((i < ny) && (j < nx) && (k < nz) && (i >= 0) && (j >= 0) && (k >= 0)){
+   	mean = mean + vec4(velocity,1.0);
+   }
+
+   gl_FragColor = mean;
+
+}
