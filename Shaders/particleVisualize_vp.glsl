82bf5aea3d29a3d8d5b94588f54ee46cca61deaa
no_author <no_author@no_author>
2007-08-16 19:08:42 +0000

    started coloring of particles

--- Shaders/particleVisualize_vp.glsl
+++ Shaders/particleVisualize_vp.glsl
@@ -1,7 +1,18 @@
+//uniform samplerRect vel;
+varying vec4 pcolor;
+
 void main(void)
 {
 	vec4 pos = vec4(gl_Vertex);
 	pos.w = 1.0;
-	gl_Color = vec4(1.0, 1.0, 1.0, 1.0);
+
+
+	pcolor = gl_Color;
+	//gl_Color = vec4(1.0, 1.0, 1.0, 1.0);
+	//pcolor = vec3(textureRect(vel,gl_TexCoord[0].xy));
+	//pcolor = normalize(pcolor);
+
 	gl_Position = gl_ModelViewProjectionMatrix * pos;
+	//gl_TexCoord[0] = gl_MultiTexCoord0;
+	//gl_Position = ftransform();
 }
