05e95395f44a96ec2a9f66ad8f286d2bf109e714
no_author <no_author@no_author>
2007-08-22 17:36:07 +0000

    changed coloring

--- Shaders/currVel_fp.glsl
+++ Shaders/currVel_fp.glsl
@@ -1,5 +1,6 @@
 uniform samplerRect currPrime;
 uniform samplerRect position;
+uniform samplerRect position_prev;
 uniform samplerRect windVel;
 
 uniform float numInRow;
@@ -12,27 +13,29 @@ void main(void)
 {
    vec2 texCoord = gl_TexCoord[0].xy;
    vec3 primeVel = vec3(textureRect(currPrime, texCoord));
-   //vec4 windV = vec4(textureRect(windVel, texCoord));
+   vec3 prevPos = vec3(textureRect(position_prev, texCoord));
    vec3 pos = vec3(textureRect(position, texCoord));
 
-   int i = floor(pos.y);
-   int j = floor(pos.x);
-   int k = floor(pos.z);
+   //int i = floor(pos.y);
+   //int j = floor(pos.x);
+   //int k = floor(pos.z);
 
-   vec4 color = vec4(1.0,1.0,1.0,1.0);
+   //vec4 color = vec4(1.0,1.0,1.0,1.0);
 
    //if((i < ny) && (j < nx) && (k < nz) && (i >= 0) && (j >= 0) && (k >= 0)){
-	vec2 index;
-	index.s = j + mod(k,numInRow)*nx;
-	index.t = i + floor(k/numInRow)*ny;
+   //vec2 index;
+   //index.s = j + mod(k,numInRow)*nx;
+   //index.t = i + floor(k/numInRow)*ny;
 	
-	vec3 wind = vec3(textureRect(windVel,index));
-	color = vec4(wind+primeVel,1.0);
-	color = abs(normalize(color));// + vec4(1.0,1.0,1.0,0.0);
-	//color = color/2.0;
+   //vec3 wind = vec3(textureRect(windVel,index));
+   //color = vec4(wind+primeVel,1.0);
+   //color = abs(normalize(color));
+   
+   vec3 color = abs(normalize(pos - prevPos));
+
    	
    //}
 
-   gl_FragColor = color;
+   gl_FragColor = vec4(color,1.0);
 
 }
