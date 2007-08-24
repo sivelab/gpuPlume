a8ff7682ca8dc18339583b5b61f30d8d4afe94a2
no_author <no_author@no_author>
2007-08-24 18:28:50 +0000

    Fixed opponent space coloring

--- Shaders/currVel_fp.glsl
+++ Shaders/currVel_fp.glsl
@@ -40,10 +40,27 @@ void main(void)
    // opposes Yellow, and Black opposes White.  We'll see how it
    // works.
 
-   mat3 opponent2rgb = mat3(1.1677, -6.4315, -0.5044, 0.9014, 2.5970, 0.0159, 0.7214, 0.1257, 2.0517);
+   // mat3 opponent2rgb = mat3(1.1677, -6.4315, -0.5044, 0.9014, 2.5970, 0.0159, 0.7214, 0.1257, 2.0517);
+   mat3 opponent2rgb = mat3(1.0, 0.1140, 0.7436, 1.0, 0.1140, -0.4111, 1.0, -0.8860, 0.1663);
    vec3 opponent_color = normalize(pos - prevPos);
    vec3 color = opponent2rgb * opponent_color;
+
+   // now, we need to apply a non-affine transform to make the red/green axis perpendicular to the blue/yellow axis
+   float pi = 3.14159;
+   float theta = atan2(color.z, color.y);
+   float theta_0;
+   if (theta < (pi/3.0))
+      theta_0 = (3.0 / 2.0) * theta;
+   else 
+      theta_0 = pi/2.0 + 0.75*(theta - pi/3.0);
    	
+   vec2 oRGB_new, oRGB;
+   oRGB = vec2(color.y, color.z);
+   mat2 rot = mat2(cos(theta), sin(theta), sin(theta), -cos(theta));
+   oRGB_new = rot * oRGB;
+   color.y = oRGB_new.x;
+   color.z = oRGB_new.y;
+
    //}
 
    gl_FragColor = vec4(color,1.0);
