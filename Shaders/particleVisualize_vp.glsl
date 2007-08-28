5d9191c6407124b864e37509fee4e3a7aa4e9a35
Pete Willemsen <willemsn@d.umn.edu>
2007-08-28 04:08:44 +0000

    updates to use point sprites

--- Shaders/particleVisualize_vp.glsl
+++ Shaders/particleVisualize_vp.glsl
@@ -1,4 +1,3 @@
-//uniform samplerRect vel;
 varying vec4 pcolor;
 
 void main(void)
@@ -6,13 +5,15 @@ void main(void)
 	vec4 pos = vec4(gl_Vertex);
 	pos.w = 1.0;
 
+	// We can control the point size so in the future, if we want individual particle sizes to 
+	// be different, then we will need to pass that information to the vertex shader.
+	//
+	// In the meantime, we will use this at the moment to scale the particle
+	// size with distance so particles farther away are smaller.
+	//	vec4 modv_vertex = gl_ModelViewMatrix * pos;
+	//	gl_PointSize = 62.0 / -modv_vertex.z;
 
 	pcolor = gl_Color;
-	//gl_Color = vec4(1.0, 1.0, 1.0, 1.0);
-	//pcolor = vec3(textureRect(vel,gl_TexCoord[0].xy));
-	//pcolor = normalize(pcolor);
 
 	gl_Position = gl_ModelViewProjectionMatrix * pos;
-	//gl_TexCoord[0] = gl_MultiTexCoord0;
-	//gl_Position = ftransform();
 }
