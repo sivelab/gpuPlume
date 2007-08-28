f01feb8cdafb24a28864e111809084ace53d9d7e
Pete Willemsen <willemsn@d.umn.edu>
2007-08-28 04:09:09 +0000

    updates to use point sprites

--- Shaders/particleVisualize_fp.glsl
+++ Shaders/particleVisualize_fp.glsl
@@ -1,20 +1,39 @@
-//uniform samplerRect render_texunit;
+uniform sampler2D pointsprite_texunit;
+uniform sampler2D pointspritenormal_texunit;
+uniform int point_visuals;
+
 varying vec4 pcolor;
 
 void main(void)
 {
-	/*	
-	vec4 pos = vec4(textureRect(render_texunit, gl_TexCoord[0].st));
-	
-	pos.x = 1.0;
-	pos.y = 1.0;
-	pos.z = 1.0;
-	pos.a = 1.0;*/
-        
-	//Draw particles white
-	//gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-	//Draw particles black
-	//gl_FragColor = vec4(0.0,0.0,0.0,1.0);
-	gl_FragColor = pcolor;
+	if (point_visuals == 1)
+	{
+		vec4 d = vec4(texture2D(pointspritenormal_texunit, gl_TexCoord[0].st));
+		vec4 normal = vec4(texture2D(pointspritenormal_texunit, gl_TexCoord[0].st));
+		if (normal.w == 0.0)
+			discard;
+		else
+		{
+		    vec4 l = normalize(vec4(1.0, -1.0, 1.0, 0.0));
+		    vec4 n = (normal * 2.0) - vec4(1.0);
+		    n.w = 0.0;
+		    vec4 h = normalize(l + vec4(0.0,0.0,1.0,0.0));
 
+		    float p = 64.0;
+		    float cp = 1.0;
+		    vec4 cr = pcolor;
+		    vec4 cl = vec4(1.0,1.0,1.0,1.0);
+		    vec4 ca = vec4(0.4,0.4,0.4,1.0);
+
+		    vec4 color = cr * (ca + cl * max(0.0,dot(n,l))) + cp * cl * pow(max(0.0,dot(h,n)), p);  
+		    gl_FragColor = color;
+		}
+	}
+	else 
+	{
+		// 
+		// If using simple point based particles, this is all that needs to be done
+		// 
+		gl_FragColor = pcolor;
+	}
 }
