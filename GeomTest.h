daf2bafcbaf6884bbb0c8e5ca5d59fac03420d6f
no_author <no_author@no_author>
2008-01-10 22:09:39 +0000

    added to geom test

--- GeomTest.h
+++ GeomTest.h
@@ -5,15 +5,6 @@
 #include <vector>
 #include "plumeControl.h"
 
-typedef struct{
-  //Index into position texture
-  int x;
-  int y;
-  //Index into pathLine texture
-  int s;
-  int t;
-}pathIdx;
-
 class GeomTest : public PlumeControl{
 
  public:
@@ -21,22 +12,13 @@ class GeomTest : public PlumeControl{
   GeomTest(Util*);
   virtual void init(bool);
   virtual int display();
-  virtual void setupTextures();
-  //virtual void setupEmitters();
+ 
   
  protected:
-  int pwidth,pheight;
-
-  std::list<pathIdx> pathList;
-
-  std::list<pathIdx>::iterator pathIter;
   
-  virtual void initFBO();
   virtual ~GeomTest();
 
   GLint uniform_postPP, uniform_posPP, uniform_x,uniform_y;
-
-  GLenum path_buffer,paths;
 
   GLSLObject geomShader,testAdvectShader, pathLineShader;
 
