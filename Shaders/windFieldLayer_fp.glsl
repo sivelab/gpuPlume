2e3bf9bec8db8646b791d0e23cce725bcd023560
no_author <no_author@no_author>
2008-01-24 16:55:07 +0000

    windFieldLayer_fp.glsl updates

--- Shaders/windFieldLayer_fp.glsl
+++ Shaders/windFieldLayer_fp.glsl
@@ -10,32 +10,60 @@ void main(void)
   vec2 texCoord = gl_TexCoord[0].xy;
   vec4 wind_dir = vec4(textureRect(Wind, texCoord)); 
   
-  mat3 opponent2rgb = mat3(1.0,  0.1140,  0.7436, 
-			   1.0,  0.1140, -0.4111, 
-			   1.0, -0.8860, 0.1663);
-  vec3 opponent_color = normalize(wind_dir);
-
+  // mat3 opponent2rgb = mat3(1.0,  0.1140,  0.7436, 
+  // 1.0,  0.1140, -0.4111, 
+  // 1.0, -0.8860, 0.1663);
+  mat3 opponent2rgb = mat3(0.299,  0.587,  0.114, 
+			   0.500,  0.500, -1.000, 
+			   0.866, -0.886,  0.000);
+  vec3 opponent_color = normalize(wind_dir).zxy;
 
   // opponent_color = normalize(wind);
 
+  opponent_color.x = (1.0 + opponent_color.x)/2.0;
+  opponent_color.y = opponent_color.y * sqrt(2.0);
+  opponent_color.z = opponent_color.z * sqrt(2.0);
+
   vec3 color = opponent2rgb * opponent_color;
 
-  // now, we need to apply a non-affine transform to make the red/green axis perpendicular to the blue/yellow axis
-  float pi = 3.14159;
-  float theta = atan(color.z, color.y);
-  float theta_0 = 0.0;
-  if (theta < (pi/3.0))
-    theta_0 = (3.0 / 2.0) * theta;
-  else if (theta <= pi && theta >= (pi/3.0))
-    theta_0 = pi/2.0 + 0.75*(theta - pi/3.0);
-      
-  vec2 oRGB_new, oRGB;
-  oRGB = vec2(color.y, color.z);
-  mat2 rot = mat2(cos(theta), -sin(theta), sin(theta), cos(theta));
-  oRGB_new = rot * oRGB;
-      
-  color.y = oRGB_new.x;  
-  color.z = oRGB_new.y;
+      float pi = 3.141592654;
+      float pi_3 = 1.047197551;
+      float theta_0 = 0.0;
+
+      vec2 oRGB_new, oRGB;
+      float theta = atan(color.z, color.y);
+      if (theta >= 0.0)
+      	{
+	  // value of theta will/should be in [0 to pi] range
+	  if (theta < pi_3)
+	    theta_0 = 1.5 * theta;
+	  else if (theta <= pi && theta >= pi_3)
+	    theta_0 = pi/4.0 + 0.75*theta;
+	  //	theta_0 = pi/2.0 + 0.75*(theta - pi/3.0);
+	  
+	  oRGB = vec2(color.y, color.z);
+	  mat2 rot = mat2(cos(theta_0 - theta), -sin(theta_0 - theta), sin(theta_0 - theta), cos(theta_0 - theta));
+	  oRGB_new = rot * oRGB;
+	}
+      else 
+	{
+	  // value of theta will/should be in [0 to -pi] range
+	  theta = -1.0 * theta;
+	  if (theta < pi_3)
+	    theta_0 = 1.5 * theta;
+	  else if (theta <= pi && theta >= pi_3)
+	    theta_0 = pi/4.0 + 0.75*theta;
+	  //	theta_0 = pi/2.0 + 0.75*(theta - pi/3.0);
+	  
+	  oRGB = vec2(color.y, color.z);
+	  theta_0 = -1.0 * theta_0;
+	  theta = -1.0 * theta;
+	  mat2 rot = mat2(cos(theta_0 - theta), -sin(theta_0 - theta), sin(theta_0 - theta), cos(theta_0 - theta));
+	  oRGB_new = rot * oRGB;
+	}
+
+      color.y = oRGB_new.x;  
+      color.z = oRGB_new.y;
 
   gl_FragColor = vec4(color,1.0);
 
