074728849f26b18f92e893001f3661b5a6937905
Pete Willemsen <willemsn@d.umn.edu>
2008-02-24 12:36:49 +0000

    update for snow vis

--- Shaders/snowVisualize_fp.glsl
+++ Shaders/snowVisualize_fp.glsl
@@ -7,29 +7,50 @@ varying vec4 pcolor;
 varying vec4 particle_pos;
 
 varying vec4 particle_epos;
-varying vec4 lpos;
+varying vec4 lpos1, lpos2, lpos3;
 
 void main(void)
 {
-  vec4 cr = vec4(1.0, 1.0, 1.0, 1.0);
-  vec4 cl = vec4(1.0,1.0,1.0,1.0);
+  vec4 cl1 = vec4(1.0,0.0,0.0,1.0);
+  vec4 cl2 = vec4(0.0,1.0,0.0,1.0);
+  vec4 cl3 = vec4(0.0,0.0,1.0,1.0);
   // vec4 ca = vec4(0.5,0.5,0.5,1.0);
   vec4 ca = vec4(0.0,0.0,0.0,1.0);
+
+  vec3 lnormal = vec3(0.0, 0.0, -1.0);
 
   // Setting g to 0.0 produces an isotropic scattering, integrated over
   // the sphere (1/4pi).  This can be used as a test case to get
   // transparency and other parameters for ambient light correct.
   // float g = 0.0;
   float g = 0.67;
-
-  vec3 w = normalize(vec3(-particle_epos));
-  vec3 wp = normalize(vec3(particle_epos.xyz - lpos.xyz));
-  float costheta = dot(w, wp);
-
   float pi = 3.141592654;
-  float phase = 1.0 / (4.0*pi) * (1.0 - g*g) / pow(1.0 + g*g - 2.0 * g * costheta, 1.5);
+  vec3 w = normalize(vec3(-particle_epos));
 
-  vec4 color = phase * cl + ca;
+  vec3 wp = normalize(vec3(particle_epos.xyz - lpos1.xyz));
+
+  // that should be the costheta for the theta between the light's normal and where the particle is relative to light
+  // so if this value is greaterthan (45 degrees), so cos(45) or 0.7071 then zero out light...
+  float vis = max(0.0, dot(lnormal, wp));
+  if (vis > 0.7071) vis = 0.0; else vis = 1.0;
+
+  float costheta = dot(w, wp);
+  float phase = 1.0 / (4.0*pi) * (1.0 - g*g) / pow(1.0 + g*g - 2.0 * g * costheta, 1.5);
+  vec4 color = phase*cl1 + ca;
+
+  wp = normalize(vec3(particle_epos.xyz - lpos2.xyz));
+  vis = max(0.0, dot(lnormal, wp));
+  if (vis > 0.7071) vis = 0.0; else vis = 1.0;
+  costheta = dot(w, wp);
+  phase = 1.0 / (4.0*pi) * (1.0 - g*g) / pow(1.0 + g*g - 2.0 * g * costheta, 1.5);
+  color = color + (phase*cl2 + ca);
+
+  wp = normalize(vec3(particle_epos.xyz - lpos3.xyz));
+  vis = max(0.0, dot(lnormal, wp));
+  if (vis > 0.7071) vis = 0.0; else vis = 1.0;
+  costheta = dot(w, wp);
+  phase = 1.0 / (4.0*pi) * (1.0 - g*g) / pow(1.0 + g*g - 2.0 * g * costheta, 1.5);
+  color = color + (phase*cl3 + ca);
 
   // simple radial expansion for alpha
   // float radius = sqrt( ((0.5-gl_TexCoord[0].s) * (0.5-gl_TexCoord[0].s)) + ((0.5-gl_TexCoord[0].t) * (0.5-gl_TexCoord[0].t)) );
--- Shaders/snowVisualize_vp.glsl
+++ Shaders/snowVisualize_vp.glsl
@@ -1,30 +1,31 @@
 varying vec4 pcolor;
 varying vec4 particle_pos;
 varying vec4 particle_epos;
-varying vec4 lpos;
+varying vec4 lpos1, lpos2, lpos3;
 
 void main(void)
 {
-	vec4 pos = vec4(gl_Vertex);
-	pos.w = 1.0;
+  vec4 pos = vec4(gl_Vertex);
+  pos.w = 1.0;
 
-	// pass the particle position to the fragment shader to perform a
-	// lookup into the turbulence field.
-	particle_pos = pos;
+  // pass the particle position to the fragment shader to perform a
+  // lookup into the turbulence field.
+  particle_pos = pos;
 
-	particle_epos = gl_ModelViewMatrix * pos;
-	lpos = gl_ModelViewMatrix * vec4(50.0, 25.0, 2.5, 1.0);
+  particle_epos = gl_ModelViewMatrix * pos;
+  lpos1 = gl_ModelViewMatrix * vec4(10.0, 20.0, 6, 1.0);
+  lpos2 = gl_ModelViewMatrix * vec4(10.0, 40.0, 6, 1.0);
+  lpos3 = gl_ModelViewMatrix * vec4(10.0, 60.0, 6, 1.0);
+  
+  pcolor = gl_Color;
 
-	pcolor = gl_Color;
-
-	// We can control the point size so in the future, if we want individual particle sizes to 
-	// be different, then we will need to pass that information to the vertex shader.
-	//
-	// In the meantime, we will use this at the moment to scale the particle
-	// size with distance so particles farther away are smaller.
-	vec4 modv_vertex = gl_ModelViewMatrix * pos;
- 	gl_PointSize = 60.0 / -modv_vertex.z;
-
-	gl_Position = gl_ModelViewProjectionMatrix * pos;
-
+  // We can control the point size so in the future, if we want individual particle sizes to 
+  // be different, then we will need to pass that information to the vertex shader.
+  //
+  // In the meantime, we will use this at the moment to scale the particle
+  // size with distance so particles farther away are smaller.
+  vec4 modv_vertex = gl_ModelViewMatrix * pos;
+  gl_PointSize = 30.0 / -modv_vertex.z;
+  
+  gl_Position = gl_ModelViewProjectionMatrix * pos;
 }
