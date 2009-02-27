4b23b0b9c6ab630ede0da2ce44b595f679cecf71
Pete Willemsen <willemsn@d.umn.edu>
2009-02-27 21:13:33 +0000

    UPdates

--- Shaders/isoSurface_gp.glsl
+++ Shaders/isoSurface_gp.glsl
@@ -1,5 +1,5 @@
-// #version 120
-// #extension GL_EXT_geometry_shader4 : enable
+#version 120
+#extension GL_EXT_geometry_shader4 : enable
 uniform sampler3D tau;
 uniform sampler1D case_to_numpoly;
 uniform sampler2D edge_connect_list;
