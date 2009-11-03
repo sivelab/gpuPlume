b32d7518e3a264613f78a2db400715bad8ad76c6
Pete Willemsen <willemsn@d.umn.edu>
2009-11-03 05:26:10 +0000

    Added a generic class that parses arguments according to the getopt functionality.  The interface to the class is simple as you provide the arguments you are interested at the beginning of the program and then have the class parse the args for you.  See the beginning of gpuPlume.cpp for a working example.

---
+++ ArgumentParsing.cpp
@@ -0,0 +1,136 @@
+/*
+ *  ArgumentParsing.cpp
+ *  NETCODE
+ *
+ *  Created by Pete Willemsen on 10/6/09.
+ *  Copyright 2009 Department of Computer Science, University of Minnesota-Duluth. All rights reserved.
+ *
+ */
+
+#include "ArgumentParsing.h"
+
+ArgumentParsing::ArgumentParsing()
+{
+};
+
+ArgumentParsing::ArgumentParsing(int argc, char *argv[])
+{
+  process(argc, argv);
+};
+
+ArgumentParsing::~ArgumentParsing()
+{
+}
+
+void ArgumentParsing::reg(const std::string& argName, char shortArgName, int has_argument, bool required)
+{
+  ModifiedOption nextArg;
+
+  // the option structure uses C-style character strings so get our "string" into that form.
+  nextArg.optParams.name = (const char *)malloc(argName.length() + 1);
+  strcpy((char *)nextArg.optParams.name, argName.c_str());
+
+  nextArg.optParams.has_arg = has_argument;
+  nextArg.optParams.flag = 0;
+  nextArg.optParams.val = shortArgName;
+
+  nextArg.isSet = false;
+  nextArg.optionalArgument = "";
+
+  m_ArgVector.push_back(nextArg);
+}
+
+bool ArgumentParsing::isSet(const std::string& argName)
+{
+  for (unsigned int i=0; i<m_ArgVector.size(); ++i)
+    if (argName == m_ArgVector[i].optParams.name)
+      return m_ArgVector[i].isSet;
+  return false;
+}
+
+bool ArgumentParsing::isSet(const std::string& argName, std::string &argValue)
+{
+  for (unsigned int i=0; i<m_ArgVector.size(); ++i)
+    if ((argName == m_ArgVector[i].optParams.name) && (m_ArgVector[i].isSet))
+      {
+	argValue = m_ArgVector[i].optionalArgument;
+	return true;
+      }
+
+  argValue = "";
+  return false;
+}
+
+int ArgumentParsing::process(int argc, char *argv[])
+{
+  // convert the vector into a temporary option struct needed for getopt_long
+  option *getoptOptions = new option[m_ArgVector.size()];
+
+  for (unsigned int i=0; i<m_ArgVector.size(); ++i)
+    {
+      getoptOptions[i].name = (const char *)malloc(strlen(m_ArgVector[i].optParams.name));
+      strcpy((char *)getoptOptions[i].name, m_ArgVector[i].optParams.name);
+      getoptOptions[i].has_arg = m_ArgVector[i].optParams.has_arg;
+      getoptOptions[i].flag = m_ArgVector[i].optParams.flag;
+      getoptOptions[i].val = m_ArgVector[i].optParams.val;
+    }
+
+  int c;
+  while (1)
+    {
+      // which option index are we on?
+      int this_option_optind = optind ? optind : 1;
+      int option_index = 0;
+
+      c = getopt_long(argc, argv, "", getoptOptions, &option_index);
+
+      // when the getopt functions return a -1, there are no more
+      // arguments to process.
+      if (c == -1)
+	break;
+
+      int argIdx = 0;
+      bool found = false;
+      while (!found && argIdx < m_ArgVector.size() && c != '?')
+	{
+	  if (c == m_ArgVector[argIdx].optParams.val)
+	    {
+	      std::cout << "found option: " << m_ArgVector[argIdx].optParams.name << std::endl;
+	      m_ArgVector[argIdx].isSet = true;
+	      if (m_ArgVector[argIdx].optParams.has_arg != no_argument)
+		{
+		  m_ArgVector[argIdx].optionalArgument = optarg;	      
+		  std::cout << "\t optarg = " << optarg << std::endl;
+		}
+	      
+	      found = true;
+	    }
+
+	  argIdx++;
+	}
+
+      if (!found)
+	{
+	  std::cerr << "?? getopt returned character code: " << c << std::endl;
+	}
+    }
+
+#if 0
+  // look over the non-option elements
+  if (optind < argc) {
+    printf("non-option ARGV-elements: ");
+    while (optind < argc)
+      printf("%s ", argv[optind++]);
+    printf("\n");
+  }
+#endif
+
+  // delete the memory we created to make this happen
+  for (unsigned int i=0; i<m_ArgVector.size(); ++i)
+    {
+      delete [] getoptOptions[i].name;
+    }
+  delete [] getoptOptions;
+
+  return 1;
+}
---
+++ ArgumentParsing.h
@@ -0,0 +1,55 @@
+/*
+ *  ArgumentParsing.h
+ *  NETCODE
+ *
+ *  Created by Pete Willemsen on 10/6/09.
+ *  Copyright 2009 Department of Computer Science, University of Minnesota-Duluth. All rights reserved.
+ *
+ */
+
+#ifndef __ARGUMENT_PARSING_H__
+#define __ARGUMENT_PARSING_H__ 1
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+#include <getopt.h>
+
+class ArgumentParsing
+{
+public:
+  ArgumentParsing();
+  ArgumentParsing(int argc, char *argv[]);
+  ~ArgumentParsing();
+
+  void reg(const std::string& argName, char shortArgName, int has_argument, bool required=false);
+  int processCommandLineArgs(int argc, char *argv[]) { return process(argc, argv); }
+
+  bool isSet(const std::string& argName);
+  bool isSet(const std::string& argName, std::string &argValue);
+
+protected:
+  int process(int argc, char *argv[]);
+	
+private:
+
+  // getopt_long structure variables
+  // struct option {
+  //    const char *name;
+  //    int         has_arg;
+  //    int        *flag;
+  //    int         val;
+  // };
+
+  struct ModifiedOption 
+  {
+    option optParams;
+    bool isSet;
+    std::string optionalArgument;
+  };
+   
+  std::vector<ModifiedOption> m_ArgVector;
+};
+
+#endif // __ARGUMENT_PARSING_H__ 1
