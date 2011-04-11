7b82d02bd83266d7a565ca2f0e2b8c39d178ef8c
Pete Willemsen <willemsn@d.umn.edu>
2011-04-11 19:50:19 +0000

    Modifying the visualization of layers to display TKE.

--- Shaders/turbulencePlane_fp.glsl
+++ Shaders/turbulencePlane_fp.glsl
@@ -13,70 +13,54 @@ uniform float slider;
 
 void main(void)
 {
-  
   vec3 texCoord = gl_TexCoord[0].xyz;
- 
   vec4 color = vec4(texture3D(Tau, texCoord));
-
     
-  vec3 c1 = vec3(0.0,0.0,1.0);
-  vec3 c2 = vec3(1.0,1.0,0.0);
-  vec3 c3 = vec3(1.0,0.0,0.0);
+  vec3 layerColor; 
+  float tke = (color.x + color.y + color.z) * 0.5;
 
-  float t;
+  float minTKE = (min11 + min22 + min33) * 0.5;
+  float maxTKE = (max11 + max22 + max33) * 0.5;
+  float rangeTKE = maxTKE - minTKE;
 
-  float max = max11;
-  if(max22>max)
-    max = max22;
-  if(max33>max)
-    max = max33;
-  if(max13>max)
-    max = max13;
-  float min = min11;
-  if(min22<min)
-    min = min22;
-  if(min33<min)
-    min = min33;
-  if(min13<min)
-    min = min13;
+  tke = (tke - minTKE) / rangeTKE;
 
-  vec3 col;
-  float range = max - min;
-  //float r = ((max-min)/2.0)/(max-min);
-  //r = 0.05;
+  vec3 DKRED = vec3(0.5, 0.0, 0.0);
+  vec3 RED = vec3(1.0, 0.0, 0.0);
+  vec3 YELLOW = vec3(1.0, 1.0, 0.0);
+  vec3 CYAN = vec3(0.0, 1.0, 1.0);
+  vec3 BLUE = vec3(0.0, 0.0, 1.0);
+  vec3 DKBLUE = vec3(0.0, 0.0, 0.5);
 
-  if(controlTau == 1){
-    //t = (color.x-min11)/(max11-min11);
-    t = (color.x-min)/range; 
-    
-  }
-  //Show only Tau22
-  else if(controlTau == 2){
-    //t = (color.y-min22)/(max22-min22);
-    t = (color.y-min)/range;
-  }
-  else if(controlTau == 3){
-    //t = (color.z-min33)/(max33-min33);
-    t = (color.z-min)/range;
-  }
-  else if(controlTau == 4){
-    //t = (color.w-min13)/(max13-min13);
-    t = (color.w-min)/range;
-  }
+  // Mapping the following
+  // Dark Blue <--> Blue <--> Cyan <--> Yellow <--> Red <--> Dark Red
+  //     0          0.125     0.375     0.625       0.875      1.0  
+  float t = 0.0;
+  if (tke <= 0.125)
+    {
+      t = tke * 8.0;  // scale to 0-1
+      layerColor = mix(DKBLUE, BLUE, t);
+    }    
+  else if (tke <= 0.375) 
+    { 
+      t = (tke - 0.125) * 4.0;
+      layerColor = mix(BLUE, CYAN, t);
+    }
+  else if (tke <= 0.625) 
+    { 
+      t = (tke - 0.375) * 4.0;
+      layerColor = mix(CYAN, YELLOW, t);
+    }
+  else if (tke <= 0.875)
+    { 
+      t = (tke - 0.625) * 4.0;
+      layerColor = mix(YELLOW, RED, t);
+    }
+  else
+    {	
+      t = (tke - 0.875) * 8.0;
+      layerColor = mix(RED, DKRED, t);
+    }
 
-  
- 
-  if(t >= slider){
-    t = (t-slider)/(1.0-slider);
-    col = c1*(1.0-t) + c2*t;
-  }
-  else{
-    t = (slider-t)/(slider);   
-    col = c3*(t) + c1*(1.0-t);
-    //gl_FragColor = c3*(1.0-t) + c1*(t);
-  }
-  
-  gl_FragColor = vec4(col,0.8);
- 
-    
+  gl_FragColor = vec4(layerColor,0.75);
 }
